1fb3399771ee90dcf029c587ae95587062ae06e8
Matt <blade2021@live.com>
2019-09-15 07:32:08 -0400

    Nightly

--- Tix/src/Ticket.java
+++ Tix/src/Ticket.java
@@ -1,71 +1,28 @@
-
+/**
+ * A class that constructs tickets for a theater
+ * @author Charlie MacFadyen
+ */
 public class Ticket {
-	
-	private String eventName;
-	private double ticketPrice;
-	private char seatRow;
-	private int seatNum;
-	
-	public Ticket() {
-		eventName = "Beyonce";
-		ticketPrice = 75.50;
-		seatRow = 'J';
-		seatNum = 32;
-	}
-	
-	public void printOut() {
-		System.out.println("******\nTICKET");
-		System.out.println(eventName);
-		System.out.println('$' + ticketPrice);
-		System.out.println("Seat " + seatRow + "-" + seatNum);
-		System.out.println("*******");
-	}
-	
-	public String getEventName() {
-		return eventName;
-	}
-
-
-
-	public void setEventName(String eventName) {
-		this.eventName = eventName;
-	}
-
-
-
-	public double getTicketPrice() {
-		return ticketPrice;
-	}
-
-
-
-	public void setTicketPrice(double ticketPrice) {
-		this.ticketPrice = ticketPrice;
-	}
-
-
-
-	public char getSeatRow() {
-		return seatRow;
-	}
-
-
-
-	public void setSeatRow(char seatRow) {
-		this.seatRow = seatRow;
-	}
-
-
-
-	public int getSeatNum() {
-		return seatNum;
-	}
-
-
-
-	public void setSeatNum(int seatNum) {
-		this.seatNum = seatNum;
-	}
+  private String eventName;
+  private double price;
+  private char row;
+  private int seatNumber;
+  
+  public Ticket(String eventName, double price, char row, int seatNumber) {
+  this.eventName = eventName;
+  this.price = price;
+  this.row = row;
+  this.seatNumber = seatNumber;
+  }
+  
+  public void printTicket() {
+    
+    System.out.println("*********");
+    System.out.println("TICKET");
+    System.out.println(eventName);
+    System.out.println("$" + price);
+    System.out.println("Seat " + row + "-" + seatNumber);
+    System.out.println("*********");
+  }
 
 }
-
--- Tix/src/TicketSeller.java
+++ Tix/src/TicketSeller.java
@@ -4,15 +4,9 @@ public class TicketSeller{
 	public static void main(String[] args) {
 		int seatNum = 2;
 		
-		Ticket game = new Ticket();
-		game.setEventName("Red Sox vs Yankees");
-		game.setTicketPrice(120.00);
-		game.setSeatRow('B');
-		
+		Ticket game = new Ticket("Red Sox vs yankees", 120.00, 'B', seatNum);
 		
 		for(int index = 0; index < 20; index++) {
-			game.setSeatNum(seatNum);
-			game.printOut();
 			seatNum += 2;
 		}
 	}
