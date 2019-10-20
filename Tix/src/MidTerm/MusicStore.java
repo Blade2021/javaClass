9a83747c1060cd480173c2296017fd41a3123100
Matt <blade2021@live.com>
2019-10-20 13:32:22 -0400

    Update

--- Tix/src/MidTerm/MusicStore.java
+++ Tix/src/MidTerm/MusicStore.java
@@ -1,45 +1,50 @@
 package MidTerm;
 
+import java.util.Scanner;
+
 public class MusicStore {
-	
+
 	private Instrument[] inventory;
 	private int storageAmount;
-	
+
 	public MusicStore(int storageAmount) {
 		this.storageAmount = storageAmount;
 		inventory = new Instrument[storageAmount];
 	}
-	
+
 	public void fillInventory() {
 		for(int indx = 0; indx < storageAmount; indx++) {
 			inventory[indx] = new Guitar((100+indx),"Taylor", "white",4);
 		}
 	}
-	
+
 	//Fill inventory amount with instrument[Guitar]
 	public void fillInventoryGuitar(int start, int end) {
-		if((start >= 0) && (end <= storageAmount)) {  //Check if array is out of bounds
-			for(int indx = start; indx <= end; indx++) {
+		for(int indx = start; indx < end; indx++) {
+			try {
 				inventory[indx] = new Guitar((100+indx),"Taylor", "white",4);
+			}
+			catch(Exception ArrayIndexOutOfBoundsException) {
+				System.out.println("Array Index exceeded");
 			}
 		}
 	}
-	
-	//Fill inventory amount with instrument[Guitar]
+
+	//Fill inventory amount with instrument[Electric Guitar]
 	public void fillInventoryElectricGuitar(int start, int end) {		
-			for(int indx = start; indx <= end; indx++) {
-				try {
-					inventory[indx] = new ElectricGuitar((100+indx),"Taylor", "white",4,'C');
-				}
-				catch(Exception ArrayIndexOutOfBoundsException) {
-					System.out.println("Array Index exceeded");
-				}
+		for(int indx = start; indx < end; indx++) {
+			try {
+				inventory[indx] = new ElectricGuitar((100+indx),"Taylor", "white",4,'C');
 			}
+			catch(Exception ArrayIndexOutOfBoundsException) {
+				System.out.println("Array Index exceeded");
+			}
+		}
 	}
-	
+
 	// Print ALL inventory
 	public void printInstrument() {
-		for(int indx = 0; indx <= inventory.length; indx++) {
+		for(int indx = 0; indx < inventory.length; indx++) {
 			try {
 				inventory[indx].printInfo();
 				System.out.println("\n ------------------- \n"); //Print separator
@@ -49,12 +54,22 @@ public class MusicStore {
 			}
 		}
 	}
-	
+
+	public void addInventory(int type) {
+		String cont = "yes";
+		int indx = 0;
+
+		do {
+			Scanner input = new Scanner(System.in);
+			System.out.println("What type of instrument are you adding?\nGuitar or Elecric Guitar");
+		} while((indx < inventory.length));
+	}
+
+
+
 	//Call printInfo method from appropriate class
 	public void printInstrument(int SerialNumber) {
 		inventory[SerialNumber].printInfo();
 	}
-	
-	
 
 }
--- Tix/src/MidTerm/Runner.java
+++ Tix/src/MidTerm/Runner.java
@@ -3,9 +3,9 @@ package MidTerm;
 public class Runner {
 
 	public static void main(String[] args) {
-		MusicStore Bennington = new MusicStore(8);
+		MusicStore Bennington = new MusicStore(7);
 		Bennington.fillInventoryGuitar(0,3);
-		Bennington.fillInventoryElectricGuitar(4,7);
+		Bennington.fillInventoryElectricGuitar(3,7);
 		Bennington.printInstrument();
 	}
 
