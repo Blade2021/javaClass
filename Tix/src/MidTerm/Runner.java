8b9a11cfabbc8135fbf480732da8ed081c9308d7
Matt <blade2021@live.com>
2019-10-20 15:46:57 -0400

    Update Runner.java

--- Tix/src/MidTerm/Runner.java
+++ Tix/src/MidTerm/Runner.java
@@ -3,24 +3,24 @@ package MidTerm;
 public class Runner {
 
 	public static void main(String[] args) {
-		MusicStore Bennington = new MusicStore(7);
-		Bennington.fillInventoryGuitar(0,3);
-		Bennington.fillInventoryElectricGuitar(3,7);
+		MusicStore Bennington = new MusicStore(7);  // Generate the inventory array
+		Bennington.fillInventoryGuitar(0,3); //Fill inventory with guitars
+		Bennington.fillInventoryElectricGuitar(3,7); //Fill inventory with electric guitars
 		
 		//Bennington.addInventory();
 		
-		Bennington.printInstrument();
-		Bennington.setAvailable();
-		Bennington.printInstrument();
+		Bennington.printInstrument();  //Print single instrument from array
+		Bennington.setAvailable(); //Set status of instrument in inventory
+		Bennington.printInstrument(); //Print single instrument from array
 		
 		
-		Bennington.setAvailable(103, true);
-		Bennington.printInstrument(103);
+		Bennington.setAvailable(103, true); //Set status of instrument in inventory (overloaded method)
+		Bennington.printInstrument(103); //Set status of instrument in inventory (overloaded method)
 		
-		Bennington.writeFile();
+		Bennington.writeFile(); //Write inventory stats to file
 		
-		Bennington.play(12);
-		Bennington.play(102);
+		Bennington.play(12); //Example of exception handling
+		Bennington.play(102); //Example of implementation without exception
 	}
 
 }
